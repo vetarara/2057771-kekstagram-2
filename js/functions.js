a1e0e97fa90030c1c15626dbc19055cd52eb8ad0
Veta <vetarara@yandex.ru>
2026-03-25 20:43:49 +0300

    комментирует вывод логов в консоль

--- js/functions.js
+++ js/functions.js
@@ -12,8 +12,8 @@ function isMeetingWithinWorkday(startDay, endDay, startMeet, duration) {
   return startMeetMin >= startDayMin && endMeetMin <= endDayMin;
 }
 
-console.log(isMeetingWithinWorkday('08:00', '17:30', '14:00', 90));
-console.log(isMeetingWithinWorkday('8:0', '10:0', '8:0', 120));
-console.log(isMeetingWithinWorkday('08:00', '14:30', '14:00', 90));
-console.log(isMeetingWithinWorkday('14:00', '17:30', '08:0', 90));
-console.log(isMeetingWithinWorkday('8:00', '17:30', '08:00', 900));
+// console.log(isMeetingWithinWorkday('08:00', '17:30', '14:00', 90));
+// console.log(isMeetingWithinWorkday('8:0', '10:0', '8:0', 120));
+// console.log(isMeetingWithinWorkday('08:00', '14:30', '14:00', 90));
+// console.log(isMeetingWithinWorkday('14:00', '17:30', '08:0', 90));
+// console.log(isMeetingWithinWorkday('8:00', '17:30', '08:00', 900));
